f7a1a9c6524dffd8fa292670d6629b6ed74f44dd
blakelobato <blakelobato1@gmail.com>
2020-02-23 13:46:46 -0800

    have user and table set up in sqlite. define and setup db

--- web_app/app.py
+++ web_app/app.py
@@ -1,5 +1,43 @@
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///twitter_app.db"
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    """Gets the user in the database"""
+    id = db.Column(db.BigInteger, primary_key=True)
+    screen_name = db.Column(db.String(128), nullable=False)
+    followers_count = db.Column(db.Integer)
+    latest_tweet_id = db.Column(db.BigInteger)
+
+class Tweet(db.Model):
+    """class to pull a user's tweets"""
+    id = db.Column(db.BigInteger, primary_key=True)
+    user_id = db.Column(db.BigInteger, db.ForeignKey("user.id"))
+    full_text = db.Column(db.String(500))
+    embedding = db.Column(db.PickleType)
+
+    user = db.relationship("User", backref=db.backref("tweets", lazy=True))
+
+
+
+
+
+
+
+
+
+
+
+
+#Routing
 
 @app.route("/")
 def hello():
